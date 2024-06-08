dc29a0d85271f4bb673d76699ce93e8c470212f8
IgorChijov <igorchijof@gmail.com>
2024-06-08 18:35:30 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author alexander = new Author("Alexander", "Pushkin");
+        Author lev = new Author("Lev", "Tolstoy");
+        Book captainsDaughter = new Book("Captains Daughter ", alexander, 1836);
+        Book warAndPeace = new Book("War and Peace ", lev, 1863);
+
+        System.out.println(captainsDaughter.getAuthor().getFirstName() + " " + captainsDaughter.getAuthor().getLastName() + " " + captainsDaughter.getTitle() +  captainsDaughter.getPublisherYear());
+        System.out.println(warAndPeace.getAuthor().getFirstName() + " " + warAndPeace.getAuthor().getLastName() + " " + warAndPeace.getTitle() + warAndPeace.getPublisherYear());
     }
+
 }
